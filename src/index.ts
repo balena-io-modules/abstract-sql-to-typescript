bc1a4af5c36732a937e28afaf52de57d29f95e28
Thodoris Greasidis <thgreasi@gmail.com>
2020-10-06 15:13:37 +0300

    Add some spacing between the generated types
    Change-type: patch

--- src/index.ts
+++ src/index.ts
@@ -79,4 +79,4 @@ export const abstractSqlToTypescriptTypes = (m: AbstractSqlModel) =>
 			const t = m.tables[tableName];
 			return tableToInterface(m, t);
 		})
-		.join('\n');
+		.join('\n\n') + '\n';
