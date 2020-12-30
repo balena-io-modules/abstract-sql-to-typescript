8267272e729853cb8e16467f1d345074d1a331fe
Pagan Gazzard <page@balena.io>
2020-12-30 18:00:59 +0000

    Add type helpers for dealing with expanded/deferred read types
    Change-type: minor

--- test/index.ts
+++ test/index.ts
@@ -18,12 +18,25 @@ const test = (
 			rules: [],
 			...model,
 		};
+		const result = abstractSqlToTypescriptTypes(t, { mode });
 
-		expect(abstractSqlToTypescriptTypes(t, { mode })).to.equal(source`
+		if (mode == null || mode === 'read') {
+			expect(result).to.equal(source`
 			export type DateString = string;
+			export type Expanded<T> = Extract<T, any[]>;
+			export type PickExpanded<T, U extends keyof T> = {
+				[P in K]: Expanded<T[P]>;
+			};
+			export type Deferred<T> = Exclude<T, any[]>;
+			export type PickDeferred<T, U extends keyof T> = {
+				[P in K]: Deferred<T[P]>;
+			};
 
 			${expectation}
 		`);
+		} else {
+			expect(result).to.equal(expectation);
+		}
 	});
 };
 
